1affe985f78745240ed81e2efc80cbbb23826714
JJP_512 <jay.perez78@yahoo.com>
2021-12-02 17:07:07 -0600

    WIP professional website

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,129 +1,67 @@
 import React, { useState } from "react";
-import Modal from "../Modal";
 
 const PhotoList = ({ category }) => {
     const [photos] = useState([
         {
-          name: 'Grocery aisle',
-          category: 'commercial',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+          name: 'Professional Head Shot',
+          category: 'about me',
+          description: 'This is a paragraph describing who I am to an employer.',
         },
         {
-          name: 'Grocery booth',
-          category: 'commercial',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+          name: 'Cactus Baby',
+          category: 'portfolio',
+          link: 'https://intense-peak-73506.herokuapp.com/main',
+          description: "MERN stack project"
         },
         {
-          name: 'Building exterior',
-          category: 'commercial',
+          name: 'Resume',
+          category: 'resume',
           description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Restaurant table',
-          category: 'commercial',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Cafe interior',
-          category: 'commercial',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Cat green eyes',
-          category: 'portraits',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Green parrot',
-          category: 'portraits',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Yellow macaw',
-          category: 'portraits',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Pug smile',
-          category: 'portraits',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Pancakes',
-          category: 'food',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Burrito',
-          category: 'food',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Scallop pasta',
-          category: 'food',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Burger',
-          category: 'food',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Fruit bowl',
-          category: 'food',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Green river',
-          category: 'landscape',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Docks',
-          category: 'landscape',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Panoramic village by sea',
-          category: 'landscape',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Domestic landscape',
-          category: 'landscape',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-        },
-        {
-          name: 'Park bench',
-          category: 'landscape',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+          link: "https://google.com"
         },
     ]);
 
     const currentPhotos = photos.filter((photo) => photo.category === category);
-    const [currentPhoto, setCurrentPhoto] = useState();
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const toggleModal = (image, i) => {
-      setCurrentPhoto({...image, index: i});
-      setIsModalOpen(!isModalOpen);
+    function hasLink(image) {
+      if (image.link) {
+        return <div>
+              {currentPhotos.map((image,i) => (
+                <a href={image.link}>
+                  {image.name}
+                </a>
+              ))}
+            </div>
+        
+      } else {
+        return <div>
+              {currentPhotos.map((image, i) => (
+                <p>
+                  {image.description}
+                </p>
+              ))}
+            </div>
+        
+      }
     }
 
     return (
         <div>
-          {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={toggleModal} />}
             <div className="flex-row">
                 {currentPhotos.map((image, i) => (
                     <img
                         src={require(`../../assets/small/${category}/${i}.jpg`).default}
                         alt={image.name}
                         className="img-thumbnail mx-1"
-                        onClick={() => toggleModal(image, i)}
+                        // onClick={() => toggleModal(image, i)}
                         key={image.name}
                     />
                 ))}
             </div>
+
+            {currentPhotos.map((image, i) => (
+              hasLink(image)
+            ))}
         </div>
     );
 }
